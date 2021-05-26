80e0aba19ce3fbef22ee621f202f6f48b355e649
TeemoTimeCodes <82971070+TeemoTimeCodes@users.noreply.github.com>
2021-05-26 15:38:56 +0100

    Added a percentage function...
    This includes:
    A new Local Variable.

--- Program.vb
+++ Program.vb
@@ -2,8 +2,8 @@ Imports System
 
 Public Module Module1
 	Public Sub Main()
-		Dim Number1 As Integer
-		Dim Number2 As Integer
+		Dim Number1 As Decimal
+		Dim Number2 As Decimal
 		Dim Number3 As Decimal
 		Dim _function As String
 		Dim _functionSign As String
@@ -54,6 +54,20 @@ Public Module Module1
 			Console.Clear()
 			Number3 = Number1 / Number2
 			Console.WriteLine("The answer to your question " & Number1 & _functionSign & Number2 & " is " & Number3)
+		ElseIf _function = "percentage" Then
+			Dim fullPercentage As Decimal
+			Console.Clear()
+			_functionSign = "%"
+			Console.WriteLine("Enter your original value and press enter!")
+			Number1 = Console.ReadLine()
+			Console.Clear()
+			Console.WriteLine("Enter your percentage's value and press enter!")
+			Number2 = Console.ReadLine()
+			fullPercentage = Number2
+			Number2 = Number2 / 100
+			Console.Clear()
+			Number3 = Number1 * Number2
+			Console.WriteLine("The answer to your question " & fullPercentage & "% of " & Number1 & " is " & Number3)
 		Else
 			Console.Clear()
 			Console.Beep()
