c14ad41a9c02ca0740ec2e3570e8011c10f4234b
TeemoTimeCodes <82971070+TeemoTimeCodes@users.noreply.github.com>
2021-05-26 15:23:51 +0100

    Added Source Code Files

---
+++ Program.vb
@@ -0,0 +1,63 @@
+Imports System
+
+Public Module Module1
+	Public Sub Main()
+		Dim Number1 As Integer
+		Dim Number2 As Integer
+		Dim Number3 As Decimal
+		Dim _function As String
+		Dim _functionSign As String
+		Console.WriteLine("Welcome to my calculator!")
+		Console.WriteLine("What function do you need the most?")
+		_function = Console.ReadLine()
+		If _function = "addition" Then
+			Console.Clear()
+			_functionSign = "+"
+			Console.WriteLine("Enter your first input and press enter!")
+			Number1 = Console.ReadLine()
+			Console.Clear()
+			Console.WriteLine("Enter your second input and press enter!")
+			Number2 = Console.ReadLine()
+			Console.Clear()
+			Number3 = Number1 + Number2
+			Console.WriteLine("The answer to your question " & Number1 & _functionSign & Number2 & " is " & Number3)
+		ElseIf _function = "subtraction" Then
+			Console.Clear()
+			_functionSign = "-"
+			Console.WriteLine("Enter your first input and press enter!")
+			Number1 = Console.ReadLine()
+			Console.Clear()
+			Console.WriteLine("Enter your second input and press enter!")
+			Number2 = Console.ReadLine()
+			Console.Clear()
+			Number3 = Number1 - Number2
+			Console.WriteLine("The answer to your question " & Number1 & _functionSign & Number2 & " is " & Number3)
+		ElseIf _function = "multiplication" Then
+			Console.Clear()
+			_functionSign = "x"
+			Console.WriteLine("Enter your first input and press enter!")
+			Number1 = Console.ReadLine()
+			Console.Clear()
+			Console.WriteLine("Enter your second input and press enter!")
+			Number2 = Console.ReadLine()
+			Console.Clear()
+			Number3 = Number1 * Number2
+			Console.WriteLine("The answer to your question " & Number1 & _functionSign & Number2 & " is " & Number3)
+		ElseIf _function = "division" Then
+			Console.Clear()
+			_functionSign = "/"
+			Console.WriteLine("Enter your first input and press enter!")
+			Number1 = Console.ReadLine()
+			Console.Clear()
+			Console.WriteLine("Enter your second input and press enter!")
+			Number2 = Console.ReadLine()
+			Console.Clear()
+			Number3 = Number1 / Number2
+			Console.WriteLine("The answer to your question " & Number1 & _functionSign & Number2 & " is " & Number3)
+		Else
+			Console.Clear()
+			Console.Beep()
+			Console.Write("The function '" & _function & "' does not exist. Please enter a valid mathematical function!")
+		End If
+	End Sub
+End Module
